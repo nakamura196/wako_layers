a4356059fffaa002067cc19128eb5632af532237
nakamura196 <na.kamura.1263@gmail.com>
2021-08-03 14:46:47 +0900

    feat: add data

---
+++ 102_trim.py
@@ -0,0 +1,40 @@
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+import json
+
+dir = "docs/2_1"
+
+with open(dir + "/config.json") as f:
+    df = json.load(f)
+
+keys = list(df.keys())
+
+key1 = keys[0]
+key2 = keys[1]
+
+img = cv2.imread(dir + "/" + key1)
+
+img2 = cv2.imread(dir + "/" + key2 + '_resized.jpeg')
+
+orgHeight, orgWidth = img.shape[:2]
+orgHeight2, orgWidth2 = img2.shape[:2]
+
+width1 = df[key1]["width"]
+width2 = df[key2]["width"]
+
+r = width1 / width2
+
+x = int(df[key2]["ow"] * r) - df[key1]["ow"]
+y = int(df[key2]["oh"] * r) - df[key1]["oh"]
+
+h = orgHeight
+w = orgWidth
+
+print(x, y, h, w)
+
+# img[top : bottom, left : right]
+# サンプル1の切り出し、保存
+img_trim = img2[y : y + h, x: x + w]
+cv2.imwrite(dir + "/" + key2 + '_trim.jpeg', img_trim)
